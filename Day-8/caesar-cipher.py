b94efc75403d9e44ff025d5a04d2bc0861890d6b
edwardma33 <edwardma33@icloud.com>
2023-01-08 00:20:08 -0500

    Done with Day-8 Project

--- Day-8/caesar-cipher.py
+++ Day-8/caesar-cipher.py
@@ -1,28 +1,45 @@
 letters = "abcdefghijklmnopqrstuvwxyz"
-
 letters_list = [*letters]
-
 letters_list += letters_list
-print(letters_list)
 
 pseudo_str = ""
 
+def decode(word, shift):
+    pseudo_str = ""
+    for i in word:
+        if i != " ":
+            pseudo_char = letters_list[letters_list.index(i) - int(shift)]
+            pseudo_str += pseudo_char
+        else:
+            pseudo_char = " "
+            pseudo_str += pseudo_char
+    
+    print(f"Decoded message: {pseudo_str}")
 
 def encrypt(word, shift):
     global pseudo_str
+    if int(shift) > 26 or len(word) == 0:
+        print("Error:\nMust be 1 or more characters\nDiplacement must be <= 26")
+        encrypt(input("Write here...\n"), input("How much displacement?\n"))
+    else:
+        for i in word:
+            if i != " ":
+                pseudo_char = letters_list[letters_list.index(i) + int(shift)]
+                pseudo_str += pseudo_char
+            else:
+                pseudo_char = " "
+                pseudo_str += pseudo_char
+        print(f"Encrypted message: {pseudo_str}")
+        if input("Decode? (y/n)\n") == "y":
+             decode(pseudo_str, shift)
 
-    if shift > 26:
-        encrypt(word, input("How much displacement?\n"))
 
-    for i in word:
-        pseudo_char = letters_list[letters_list.index(i) + int(shift)]
-        pseudo_str += pseudo_char
-    print(pseudo_str)
+if input("Would you like to encrypt or decode a message?\ne: encrypt\nor\nd: decode") == "e":
+    encrypt(input("Write here...\n"), input("How much displacement?\n"))
+else:
+    decode(input("Write here...\n"), input("How much displacement?\n"))
 
-def decode():
-    print("y'all hoes")
 
-encrypt(input("Write here...\n"), input("How much displacement?\n"))
 
 
 
