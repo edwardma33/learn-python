e7a1998b9b5fbcd721bf0846902e7413aea8ada2
edwardma33 <edwardma33@icloud.com>
2023-01-12 01:05:13 -0500

    sandy

---
+++ sandbox/point-plot.py
@@ -0,0 +1,25 @@
+import os
+def clear():
+    os.system("clear")
+clear()
+
+grid = []
+
+for i in range(0, 10):
+    grid.append([])
+
+print(len(grid))
+
+for i in range(0, len(grid)):
+    for i in range(0, 10):
+        grid[i].append("_")
+
+def plot_point(x, y):
+    grid[y][x] = "o"
+    
+
+while input("Would you like to plot a point? [y/n]: ") == "y":
+    plot_point(int(input("x: ")), int(input("y: ")))
+    for i in grid:
+        print(f"{i}")
+
