6a3b8337b81aca815aa59f0d3bb32104725d868f
edwardma33 <edwardma33@icloud.com>
2023-01-13 14:15:30 -0500

    Start wordle

---
+++ sandbox/wordle/main.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+words = ["about", "above", "hello", "world"]
+
+def pick_word():
+    return words[random.randint(0, len(words) - 1)] 
+
+
+picked_word = pick_word()
+picked_word = [*picked_word]
+
+
+print(picked_word)
