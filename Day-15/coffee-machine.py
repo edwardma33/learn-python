b9a631793a7d4eaa2bb6542e66327c4b1641f773
edwardma33 <edwardma33@icloud.com>
2023-01-16 23:27:40 -0500

    Work on Coff

--- Day-15/coffee-machine.py
+++ Day-15/coffee-machine.py
@@ -2,7 +2,13 @@ import os
 import machine_data
 
 data = machine_data
+resources = data.resources
 
-report = {
-    
-}
+def get_report():
+    output = ""
+    for i in resources:
+        output += (f"{i}: {resources[i]}\n")
+    return output
+report = get_report()
+
+
